2e0f3bd8401f5788fd0723ce3959ae84e8bfec36
Pratyush Shivam <pratyushshivam7@gmail.com>
2020-12-31 19:50:57 +0530

    Add files via upload

---
+++ mine.js
@@ -0,0 +1,38 @@
+function functions(){
+ 
+    // document.getElementsByClassName("house").innerHTML="jj"
+    console.log(document.getElementsByClassName("house")[0].innerHTML)
+    document.getElementsByClassName("house")[0].innerHTML="VIEW"
+
+}
+function functionss(){
+   alert("hh")
+
+
+}
+
+function func(){
+    window.location.href = "/index2.html";
+
+}
+
+function funcone(){
+    document.getElementsByClassName("house")[1].innerHTML="VIEW"
+
+}
+
+function functwo(){
+    document.getElementsByClassName("house")[2].innerHTML="VIEW"
+
+
+}
+function functhree(){
+    document.getElementsByClassName("house")[3].innerHTML="VIEW"
+
+
+}
+function funcfour(){
+    document.getElementsByClassName("house")[4].innerHTML="VIEW"
+
+
+}
